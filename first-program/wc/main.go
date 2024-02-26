5ae91293ad54ce271a8b1533da80962ce35a49f1
achaari <achaari.jaouad@gmail.com>
2024-02-26 23:48:05 +0100

    add basic flaging consept

--- first-program/wc/main.go
+++ first-program/wc/main.go
@@ -2,25 +2,52 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"os"
 )
 
 func main() {
-	fmt.Println(count(os.Stdin))
+	// Defining a boolean flag -l to count lines instead of words
+	lines := flag.Bool("l", false, "Count lines")
+
+	// Defining a boolean flag -b to count the number of bytes in addition to words and lines
+	bytes := flag.Bool("b", false, "Count Bytes")
+
+	// Parsing the flags provided by the user
+	flag.Parse()
+
+	fmt.Println(count(os.Stdin, *lines, *bytes))
 }
 
-func count(r io.Reader) int {
+func count(r io.Reader, countLines, bytes bool) int {
+	// A scanner is used to read text from a Reader (such as files)
 	scanner := bufio.NewScanner(r)
 
-	scanner.Split(bufio.ScanWords)
+	// If the count lines flag is not set, we want to count words so we define
+	//the scanner split type to words (default is split by lines)
+	if !countLines {
+		scanner.Split(bufio.ScanWords)
+	}
 
+	// Defining a counter
 	wc := 0
 
+	// Buffer to accumulate bytes
+	var buffer []byte
+
+	// For every word or line scanned, add 1 to the counter
 	for scanner.Scan() {
+		line := scanner.Bytes()
+		buffer = append(buffer, line...)
 		wc++
 	}
 
+	if bytes {
+		return len(buffer)
+	}
+
+	// Return the total
 	return wc
 }
--- first-program/wc/main_test.go
+++ first-program/wc/main_test.go
@@ -5,13 +5,34 @@ import (
 	"testing"
 )
 
-func TestCountWord(t *testing.T) {
+// TestCountWords tests the count function set to count words
+func TestCountWords(t *testing.T) {
 	b := bytes.NewBufferString("word1 word2 word3 word4\n")
 
 	exp := 4
 
-	res := count(b)
+	res := count(b, false, false)
 
+	if res != exp {
+		t.Errorf("Expected %d, got %d instead.\n", exp, res)
+	}
+}
+
+// TestCountLines tests the count function set to count lines
+func TestCountLines(t *testing.T) {
+	b := bytes.NewBufferString("word1 word2 word3\nline2\nline3 word1")
+	exp := 3
+	res := count(b, true, false)
+	if res != exp {
+		t.Errorf("Expected %d, got %d instead.\n", exp, res)
+	}
+}
+
+// TestCountBytes tests the count function set to count bytes
+func TestCountBytes(t *testing.T) {
+	b := bytes.NewBufferString("word1 word2 word3 word4")
+	exp := len(b.Bytes())
+	res := count(b, true, true)
 	if res != exp {
 		t.Errorf("Expected %d, got %d instead.\n", exp, res)
 	}
