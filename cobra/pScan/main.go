b16812fbbefbc03db7588e8862596dbfeccb74c2
Achaari <achaari.jaouad@gmail.com>
2024-05-06 01:16:34 +0100

    new complete project practice cobra (scan hosts with diff ports)

---
+++ cobra/pScan/main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+
+*/
+package main
+
+import "github.com/jawad-ch/Command-line-applications-in-go/cobra/pScan/cmd"
+
+func main() {
+	cmd.Execute()
+}
